9e9cb8d597839e4851056f1fca9f8f675b266937
Artyom <itcoding76@gmail.com>
2024-02-11 20:06:05 +0200

    Java Sping Lecture03

---
+++ lecture03/MyWebAplication/src/main/java/ru/gb/spring/MyWebAplication/MyWebAplicationApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.MyWebAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAplicationApplication.class, args);
+	}
+
+}
---
+++ lecture03/MyWebAplication/src/test/java/ru/gb/spring/MyWebAplication/MyWebAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.MyWebAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
