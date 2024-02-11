098736acc1376179355233417eb49694c7d33145
Artyom <itcoding76@gmail.com>
2024-02-11 20:06:44 +0200

    Java Sping Lecture04 SimplePage

---
+++ lecture04/SimplePage/src/main/java/ru/gb/spring/SimplePage/SimplePageApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.SimplePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePageApplication.class, args);
+	}
+
+}
---
+++ lecture04/SimplePage/src/test/java/ru/gb/spring/SimplePage/SimplePageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.SimplePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
