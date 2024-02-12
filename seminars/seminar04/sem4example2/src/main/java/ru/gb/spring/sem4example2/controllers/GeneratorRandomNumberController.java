aef694a70ac15c6b52dae1c1828ed921991a9185
Artyom <itcoding76@gmail.com>
2024-02-12 20:31:36 +0200

    Seminar 04 ex02  limited random number WebApplication other method sent params

--- seminars/seminar04/sem4example2/src/main/java/ru/gb/spring/sem4example2/controllers/GeneratorRandomNumberController.java
+++ seminars/seminar04/sem4example2/src/main/java/ru/gb/spring/sem4example2/controllers/GeneratorRandomNumberController.java
@@ -3,6 +3,7 @@ package ru.gb.spring.sem4example2.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Random;
@@ -19,6 +20,27 @@ public class GeneratorRandomNumberController {
 
     @GetMapping("/range-random") // http://localhost:8080/range-random?min=1&max=5
     public String rangeRandomNumber(@RequestParam(name="min") int min, @RequestParam(name="max") int max, Model model){
+        int randomNumber = new Random().nextInt(min, max + 1);
+        model.addAttribute("min", min);
+        model.addAttribute("max", max);
+        model.addAttribute("randomNumber", randomNumber);
+        return "range-randomNumber";
+    }
+
+    @GetMapping("/range-random/{min}/{max}") // http://localhost:8080/range-random/5/20
+    public String newRangeRandomNumber(@PathVariable(name="min") int min,
+                                    @PathVariable(name="max") int max,
+                                    Model model){
+        int randomNumber = new Random().nextInt(min, max + 1);
+        model.addAttribute("min", min);
+        model.addAttribute("max", max);
+        model.addAttribute("randomNumber", randomNumber);
+        return "range-randomNumber";
+    }
+    @GetMapping("/range-random/{min}-{max}") // http://localhost:8080/range-random/5-200
+    public String otherRangeRandomNumber(@PathVariable(name="min") int min,
+                                       @PathVariable(name="max") int max,
+                                       Model model){
         int randomNumber = new Random().nextInt(min, max + 1);
         model.addAttribute("min", min);
         model.addAttribute("max", max);
