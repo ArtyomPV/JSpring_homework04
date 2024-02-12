7ee35038920c2a188e6e2d749c94d1b1556a01fd
Artyom <itcoding76@gmail.com>
2024-02-12 07:06:12 +0200

    Seminar 04 initial commit

---
+++ seminars/seminar04/sem4example1/src/main/java/ru/gb/spring/sem4example1/Sem4example1Application.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.sem4example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4example1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4example1Application.class, args);
+	}
+
+}
---
+++ seminars/seminar04/sem4example1/src/test/java/ru/gb/spring/sem4example1/Sem4example1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.sem4example1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4example1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
