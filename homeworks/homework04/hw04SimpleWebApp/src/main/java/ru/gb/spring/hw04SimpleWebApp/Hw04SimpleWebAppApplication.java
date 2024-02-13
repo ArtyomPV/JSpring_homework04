5a173816f4544e733a73b662d4cb1e4dafe2fe38
Artyom <itcoding76@gmail.com>
2024-02-13 07:01:20 +0200

    JavaSpring HW 04 init commit

---
+++ homeworks/homework04/hw04SimpleWebApp/src/main/java/ru/gb/spring/hw04SimpleWebApp/Hw04SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.hw04SimpleWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw04SimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw04SimpleWebAppApplication.class, args);
+	}
+
+}
---
+++ homeworks/homework04/hw04SimpleWebApp/src/test/java/ru/gb/spring/hw04SimpleWebApp/Hw04SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.hw04SimpleWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw04SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
