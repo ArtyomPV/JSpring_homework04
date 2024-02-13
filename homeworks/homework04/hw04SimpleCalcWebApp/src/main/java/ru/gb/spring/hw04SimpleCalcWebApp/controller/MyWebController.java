48f62b3085647548c650b2148e4aebf6af11dcc9
Artyom <itcoding76@gmail.com>
2024-02-13 07:44:21 +0200

    JavaSpring HW 04 add RequestMapping

--- homeworks/homework04/hw04SimpleCalcWebApp/src/main/java/ru/gb/spring/hw04SimpleCalcWebApp/controller/MyWebController.java
+++ homeworks/homework04/hw04SimpleCalcWebApp/src/main/java/ru/gb/spring/hw04SimpleCalcWebApp/controller/MyWebController.java
@@ -3,13 +3,15 @@ package ru.gb.spring.hw04SimpleCalcWebApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Random;
 
 @Controller
+@RequestMapping("/sum2numbers")
 public class MyWebController
 {
-    @GetMapping("")
+    @GetMapping
     public String addTwoNumbers(Model model){
         int num1 = new Random().nextInt(1, 10);
         int num2 = new Random().nextInt(1, 10);
@@ -17,6 +19,6 @@ public class MyWebController
         model.addAttribute("num1", num1);
         model.addAttribute("num2", num2);
         model.addAttribute("sum", sum);
-        return "index";
+        return "sum2numbers.html";
     }
 }
