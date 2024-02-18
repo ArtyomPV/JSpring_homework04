f5c8b59b8192bc08d0af30cf0518f71e346a2eff
Artyom <itcoding76@gmail.com>
2024-02-18 21:07:48 +0200

    JavaSpring HW 05 initial commit

---
+++ homeworks/homework05/hw05SimpleWebpageDB/src/main/java/ru/gb/hw05SimpleWebpageDB/Hw05SimpleWebpageDbApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.hw05SimpleWebpageDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw05SimpleWebpageDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw05SimpleWebpageDbApplication.class, args);
+	}
+
+}
