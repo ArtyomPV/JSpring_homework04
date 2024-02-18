4db00abd436b8d2b4281b06b01c0bcf57f17f94c
Artyom <itcoding76@gmail.com>
2024-02-18 07:09:49 +0200

    JavaSpring Seminar 04

--- seminars/seminar04/sem4example2/src/main/java/ru/gb/spring/sem4example2/controllers/GeneratorRandomNumberController.java
+++ seminars/seminar04/sem4example2/src/main/java/ru/gb/spring/sem4example2/controllers/GeneratorRandomNumberController.java
@@ -1,4 +1,4 @@
-package ru.gb.spring.sem4example2.controllers;
+    package ru.gb.spring.sem4example2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
