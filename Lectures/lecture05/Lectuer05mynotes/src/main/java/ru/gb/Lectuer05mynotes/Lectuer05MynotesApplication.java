3832d7c530a5e284d4f39965dc904b0852928a66
Artyom <itcoding76@gmail.com>
2024-02-20 06:25:48 +0200

    lecture 05 mynotes init commit

---
+++ Lectures/lecture05/Lectuer05mynotes/src/main/java/ru/gb/Lectuer05mynotes/Lectuer05MynotesApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.Lectuer05mynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lectuer05MynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lectuer05MynotesApplication.class, args);
+	}
+
+}
---
+++ Lectures/lecture05/Lectuer05mynotes/src/test/java/ru/gb/Lectuer05mynotes/Lectuer05MynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.Lectuer05mynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lectuer05MynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
